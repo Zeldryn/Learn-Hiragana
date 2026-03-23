f5ad9b2ce5a4d742b919d7c924a1fe1e486fb61a
Zeldryn <ahm.riz666@gmail.com>
2026-03-24 00:58:11 +0700

    i change layout from move to layout in header

--- Widgets/Widget.py
+++ Widgets/Widget.py
@@ -1,20 +1,29 @@
-from PySide6.QtWidgets import QWidget, QGraphicsOpacityEffect
+from PySide6.QtWidgets import QWidget, QGraphicsOpacityEffect,QHBoxLayout
 from PySide6.QtCore import QRect, QPropertyAnimation,Qt
 
-class Line(QWidget):
-    def __init__(self,parent):
+class Header(QWidget):
+    def __init__(self,parent,text):
         super().__init__()
         self.setObjectName("line")
         self.parent = parent
         self.setAttribute(Qt.WA_StyledBackground,True)
-
+        self.QHBox = QHBoxLayout(self)
+        self.text = text
         self.setStyleSheet("""#line {
-                           background-color : #BBE0EF;
-                           border: 2px solid #BBE0EF;
+                           background-color : transparent;
+                           border: 2px solid transparent;
+                           border-bottom: 2px solid #0992C2;
                            border-radius: 2px;
                            }""")
         self.anim = QPropertyAnimation(self, b"geometry")
         self.anim.setDuration(1000)
-        self.anim.setStartValue(QRect(self.parent.width() // 2,self.parent.height() * 0.05,0,4))
-        self.anim.setEndValue(QRect(0,self.parent.height() * 0.05, self.parent.width(),4))
+        self.anim.setStartValue(QRect(self.parent.width() // 2,0,0,self.parent.height() * 0.05))
+        self.anim.setEndValue(QRect(0,0, self.parent.width(),self.parent.height() * 0.05))
+        self.anim.finished.connect(self.Text)
         self.anim.start()
+
+    
+    def Text(self):
+        self.QHBox.addWidget(self.text,alignment=Qt.AlignCenter)
+        self.text.anim.start()
+
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from PySide6.QtWidgets import QWidget,QApplication, QGraphicsOpacityEffect
 from Widgets.myButton import Button
-from Widgets.Widget import Line
+from Widgets.Widget import Header
 from Widgets.myLabel import Label
 from PySide6.QtCore import QRect, QPropertyAnimation
-from PySide6.QtGui import QFontMetrics
+
 
 class Main(QWidget):
     def __init__(self):
@@ -17,22 +17,11 @@ class Main(QWidget):
                            background-color: #EBF4F6}""")
         self.firstBtn = Button("firstMode",self)
         self.headLabel = Label("headLabel",self)
-        self.line = Line(self)
+        self.Header = Header(self,self.headLabel)
 
+        self.Header.setParent(self)
+        self.Header.show()
 
-        
-        self.metrics = QFontMetrics(self.headLabel.font())
-        self.headLabel.setParent(self)
-        self.headLabel.move((self.width() - self.metrics.horizontalAdvance(self.headLabel.text())) // 2 ,10)
-        self.headLabel.show()
-
-
-        self.line.setParent(self)
-        self.line.anim.finished.connect(self.startAnimationText)
-        self.line.show()
-
-    def startAnimationText(self):
-        self.headLabel.anim.start()
 
 app = QApplication([])
 window = Main()
